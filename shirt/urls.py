5ce89208ca4f686d6f156fa6c7b5d790d26cdab0
Kowalski <86694930+FelipeNascimento0@users.noreply.github.com>
2022-04-10 16:47:18 -0300

    added user authentication and permissions

--- shirt/urls.py
+++ shirt/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from shirt import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 router = DefaultRouter()
@@ -11,5 +15,7 @@ router.register('purchase', views.purchase.PurchaseViewSet, basename="purchase" 
 
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("", include(router.urls))
 ]
